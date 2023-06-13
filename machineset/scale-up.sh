30f6b1fc6c06a7bd7b3a85316c02360cd6869d6b
fajjarnr <zfajart@gmail.com>
2023-06-13 22:33:01 +0700

    adding crontab to scale up & down machine

---
+++ machineset/scale-up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+oc login -u jay -p rahasia https://api.cluster-hvt9q.hvt9q.sandbox1172.opentlc.com:6443
+oc project openshift-machine-api
+oc scale --replicas=2 machineset cluster-hvt9q-2sbxc-worker-ap-southeast-1b
