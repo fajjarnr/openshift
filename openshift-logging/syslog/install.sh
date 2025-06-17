f220f76f9d769fd5090fff3f5c1edfe0ad914ae0
fajjarnr <zfajart@gmail.com>
2025-06-17 11:12:04 +0700

    openshift logging

---
+++ openshift-logging/syslog/install.sh
@@ -0,0 +1,13 @@
+hostnamectl set-hostname syslog
+sudo apt update; sudo apt upgrade -y
+apt list -a rsyslog
+sudo apt install rsyslog -y
+sudo systemctl enable --now rsyslog
+sudo systemctl status rsyslog
+vim /etc/rsyslog.conf
+sudo systemctl restart rsyslog
+sudo systemctl status rsyslog
+ss -anlp | grep 514
+rsyslogd -f /etc/rsyslog.conf -N1
+tail -f /var/log/syslog
+ls -lh /var/log/syslog
