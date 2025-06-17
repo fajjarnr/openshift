1e3105e17bbbbc22600ded689ed10c84fb0ccdbe
fajjarnr <zfajart@gmail.com>
2025-06-17 11:15:46 +0700

    install syslog

--- openshift-logging/syslog/install.sh
+++ openshift-logging/syslog/install.sh
@@ -1,10 +1,12 @@
-hostnamectl set-hostname syslog
-sudo apt update; sudo apt upgrade -y
-apt list -a rsyslog
+#!/bin/bash
+
+sudo hostnamectl set-hostname syslog
+sudo apt update && sudo apt upgrade -y
+sudo apt list -a rsyslog
 sudo apt install rsyslog -y
 sudo systemctl enable --now rsyslog
 sudo systemctl status rsyslog
-vim /etc/rsyslog.conf
+sudo vim /etc/rsyslog.conf
 sudo systemctl restart rsyslog
 sudo systemctl status rsyslog
 ss -anlp | grep 514
